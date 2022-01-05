7fbd72620baed38217afe7e241a73d145c43f163
MonkieDragon <clivejeffries@hotmail.com>
2022-01-05 17:09:00 +0000

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import CssBaseline from "@mui/material/CssBaseline";
+import { GifBoxOutlined } from "@mui/icons-material";
 
 const Header = () => {
 	return (
 		<>
 			<CssBaseline />
 			<AppBar position="sticky">
+				<GifBoxOutlined sx={{ fontSize: "10rem" }} />
 				<Typography variant="h1" gutterBottom="true" align="center">
 					Gif App
 				</Typography>
